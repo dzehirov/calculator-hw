ce7660a0904b9cd21f29bfadd3ddadd450538a7f
dzehirov <dvz2@njit.edu>
2019-10-31 19:45:18 -0400

    Task: Successful test of subtraction code

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,8 +1,19 @@
+from CsvReader import CsvReader
+
+
 def addition(a, b):
     return a + b
 
+
 def subtraction(a, b):
-    return a - b
+    a = int(a)
+    b = int(b)
+    return b - a
+
+def mean(data):
+    mean = data
+    return mean
+
 
 class Calculator:
     result = 0
@@ -17,3 +28,14 @@ class Calculator:
     def subtract(self, a, b):
         self.result = subtraction(a, b)
         return self.result
+
+
+class CSVStats(Calculator):
+    data = []
+
+    def __init__(self, data_file):
+        self.data = CsvReader(data_file)
+        pass
+
+    def mean(self):
+        mean(self.data)
